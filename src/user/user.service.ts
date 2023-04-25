052d5e8682786e270ef734aec0f5c812a72c4c64
Dmitriy <littlebig11234@gmail.com>
2023-04-26 00:23:37 +0300

    add user-service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { userDto } from './user.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
 
+    constructor(
+        private prisma: PrismaService
+    ) {
 
-    getProfile(id: number) {
-        return {"hello": "sdfsdf"}
+    }
+
+    async getProfile(id: number) {
+        const existUser = await this.prisma.user.findUnique({
+            where: {
+                id
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                avatarPath: true,
+                password: true,
+                phone: true,
+                favorites: {
+                    select: {
+                        id: true,
+                        name: true,
+                        pr
+                    }
+                }
+            }
+        })
+
+        return existUser
     }
 
     updateProfile(id: number, dto: userDto) {
